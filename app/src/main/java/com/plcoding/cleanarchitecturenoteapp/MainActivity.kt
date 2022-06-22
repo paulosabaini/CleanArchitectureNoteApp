86809f9e5956eba1ea63ff0f8c02707ff10e76ee
Paulo Sabaini <pasabaini@gmail.com>
2022-06-22 16:57:36 -0300

    creation of modules and note feature data and domain layers

--- app/src/main/java/com/plcoding/cleanarchitecturenoteapp/MainActivity.kt
+++ app/src/main/java/com/plcoding/cleanarchitecturenoteapp/MainActivity.kt
@@ -3,13 +3,10 @@ package com.plcoding.cleanarchitecturenoteapp
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
-import androidx.compose.material.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.tooling.preview.Preview
-import com.plcoding.cleanarchitecturenoteapp.ui.theme.CleanArchitectureNoteAppTheme
+import com.plcoding.core.presentation.ui.theme.CleanArchitectureNoteAppTheme
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
---
+++ core/src/main/java/com/plcoding/core/presentation/components/Example.kt
@@ -0,0 +1,10 @@
+package com.plcoding.core.presentation.components
+
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+
+// Example composable to represent the usage of the core module
+@Composable
+fun Example() {
+    Text("Example")
+}
